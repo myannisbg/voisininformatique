77c966bee160ea95d73826b25cd1ee08998dc87c
myannisbg <503p2.yannis.mercier@gmail.com>
2025-03-26 14:43:42 +0100

    Fix db connection for PostgreSQL & Render

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,34 +1,25 @@
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv'; 
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-// // Tester la connexion à la base de données
-// const connection = async () => {
-//     let pool;
-//     try {
-//         // Créer un pool de connexions MySQL
-//         pool = mysql.createPool({
-//             host: process.env.DB_HOST,
-//             user: process.env.DB_USER,
-//             password: process.env.DB_PASSWORD,
-//             database: process.env.DB_NAME,
-//             waitForConnections: true,
-//             connectionLimit: 10,
-//             queueLimit: 0,
-//         });
-//     }
-//     catch (err) {
-//         console.error("Erreur de connexion à la base de données :", err.message);
-//     }
-//     try {
-//         const connection = await pool.getConnection();
-//         console.log("Connexion à la base de données réussie !");
-//         connection.release(); // Libère la connexion
-//     } catch (err) {
-//         console.error("Erreur de connexion à la base de données :", err.message);
-//     }
-// };
+// dotenv.config();
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'mysql', // ou 'sqlite', 'postgres', etc.
+// });
 
-// export default connection;
+// // Tester la connexion à la base de données avec Sequelize
+// // (async () => {
+// //   try {
+// //     await sequelize.authenticate();
+// //     console.log('Connexion à la base de données réussie avec Sequelize !');
+// //   } catch (err) {
+// //     console.error('Erreur de connexion à la base de données avec Sequelize :', err.message);
+// //   }
+// // })();
+
+// export default sequelize;
+
+
 
 
 
@@ -36,19 +27,16 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql', // ou 'sqlite', 'postgres', etc.
-});
 
-// Tester la connexion à la base de données avec Sequelize
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connexion à la base de données réussie avec Sequelize !');
-//   } catch (err) {
-//     console.error('Erreur de connexion à la base de données avec Sequelize :', err.message);
-//   }
-// })();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default sequelize;
